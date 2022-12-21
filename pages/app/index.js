57525f85cda75f5da74a010625d6390d41663db1
Andika Wiratana <andikawiratana@gmail.com>
2022-12-21 23:43:57 +0800

    integrate login page

--- pages/app/index.js
+++ pages/app/index.js
@@ -9,9 +9,9 @@ export default function Dashboard(){
 					<div className="flex-col form mx-auto w-2/3 bg-gray-100 p-5 rounded-xl shadow flex">
 						<div className="w-1/2 p-[20px] bg-sky-500 text-white rounded mx-auto text-center my-[20px]">2000/4000</div>
 						<div className="text-container flex justify-around">
-							<Link href="/app/food" className="bg-sky-900 px-[10px] py-[5px] hover:rounded-3xl duration-300 rounded-md hover:bg-red-500 text-white">Food</Link
-				            <Link href="/app/water" className="bg-sky-900 px-[10px] py-[5px] hover:rounded-3xl duration-300 rounded-md hover:bg-red-500 text-white">Water</Link
-				            <Link href="/app/excercise" className="bg-sky-900 px-[10px] py-[5px] hover:rounded-3xl duration-300 rounded-md hover:bg-red-500 text-white">Excercise</Link
+							<Link href="/app/food" className="bg-sky-900 px-[10px] py-[5px] hover:rounded-3xl duration-300 rounded-md hover:bg-red-500 text-white">Food</Link>
+				            <Link href="/app/water" className="bg-sky-900 px-[10px] py-[5px] hover:rounded-3xl duration-300 rounded-md hover:bg-red-500 text-white">Water</Link>
+				            <Link href="/app/excercise" className="bg-sky-900 px-[10px] py-[5px] hover:rounded-3xl duration-300 rounded-md hover:bg-red-500 text-white">Excercise</Link>
 		        		</div>
 					</div>
 				</div>
--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,33 +1,83 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function Login(){
-	return (
-		<section>
-			<div className="bg-form h-screen bg-gray-800 flex items-center">
-				<div className="container mx-auto">
-					<div className="form mx-auto w-1/3 bg-gray-100 p-5 rounded-xl shadow">
-						<div className="text-container text-center">
-							<h1 className="text-sky-900">Login</h1>
-						</div>
-						<div className="input-container text-center mx-auto">
-							<div className="input-group my-3">
-								<label htmlFor="username" className="mr-2">username</label>
-								<input id="username" type="text" name="username" className="bg-gray-200 rounded-md p-1"/>
-							</div>
-							<div className="input-group my-3">
-								<label htmlFor="password" className="mr-2">password</label>
-								<input id="password" type="text" name="password" className="bg-gray-200 rounded-md p-1"/>
-							</div>
-							<div className="input-group my-3">
-								<Link href="/app/initial-setup/form" className="bg-sky-500 rounded-md px-2 py-1 text-gray-100">Submit</Link>
-							</div>
-							<div className="text-container">
-								Don't have account ? <Link href="/auth/register" className="text-sky-900">Let's register</Link>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+    const router = useRouter()
+    
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+
+        const data = {
+          name: event.target.name.value,
+          password: event.target.password.value,
+        }
+        
+        const JSONdata = JSON.stringify(data)
+        let endpoint = 'http://localhost:5000/auth/login'
+        let options = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSONdata,
+        }
+        
+        await fetch(endpoint, options)
+        .then((res) => res.json())
+        .then(async (data) => {
+            localStorage.setItem("user_id", data.userId)
+            localStorage.setItem("access-token", data.accessToken)
+            localStorage.setItem("refresh-token", data.refreshToken)
+        })
+        
+        endpoint = `http://localhost:5000/users/detail/${localStorage.getItem("user_id")}`
+        options = {
+          method: 'GET',
+          headers: {
+            'authorization': `Bearer ${localStorage.getItem("access-token")}`,
+          },
+        }
+        
+        await fetch(endpoint, options)
+        .then((res) => res.json())
+        .then(async (data) => {
+            localStorage.setItem("user", data)
+            console.log("babi satu ini")
+            if(!data.detail.initialization_status)
+                router.push('/app/initial-setup/form')
+            router.push('/app')
+        })
+    }
+    
+    return (
+        <section>
+            <div className="bg-form h-screen bg-gray-800 flex items-center">
+                <div className="container mx-auto">
+                    <div className="form mx-auto w-1/3 bg-gray-100 p-5 rounded-xl shadow">
+                        <div className="text-container text-center">
+                            <h1 className="text-sky-900">Login</h1>
+                        </div>
+                        <form onSubmit={handleSubmit}>  
+                            <div className="input-container text-center mx-auto">
+                                <div className="input-group my-3">
+                                    <label htmlFor="name" className="mr-2">username</label>
+                                    <input id="name" type="text" name="name" className="bg-gray-200 rounded-md p-1"/>
+                                </div>
+                                <div className="input-group my-3">
+                                    <label htmlFor="password" className="mr-2">password</label>
+                                    <input id="password" type="text" name="password" className="bg-gray-200 rounded-md p-1"/>
+                                </div>
+                                <div className="input-group my-3">
+                                    <button type="submit" className="bg-sky-500 rounded-md px-2 py-1 text-gray-100">Submit</button>
+                                </div>
+                                <div className="text-container">
+                                    Don&apos;t have account ? <Link href="/auth/register" className="text-sky-900">Let&apos;s register</Link>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
