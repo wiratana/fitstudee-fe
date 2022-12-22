d81a522f90d48d8d7a96348d1c171440bb841511
Andika Wiratana <andikawiratana@gmail.com>
2022-12-22 18:47:31 +0800

    integrate register page

--- pages/auth/register.js
+++ pages/auth/register.js
@@ -1,6 +1,35 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function Register(){
+    const router = useRouter()
+    
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+
+        const data = {
+          name: event.target.name.value,
+          email: event.target.email.value,
+          password: event.target.password.value,
+          detail:{initialization_status:false}
+        }
+        
+        const JSONdata = JSON.stringify(data)
+        let endpoint = 'http://localhost:5000/users/register'
+        let options = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSONdata,
+        }
+        
+        await fetch(endpoint, options)
+        .then((res) => {
+            if(res.status == 200) router.push('/auth/login')
+        })
+    }
+    
 	return (
 		<section>
 			<div className="bg-form h-screen bg-gray-800 flex items-center">
@@ -9,26 +38,28 @@ export default function Register(){
 						<div className="text-container text-center">
 							<h1 className="text-sky-900">Register</h1>
 						</div>
-						<div className="input-container text-center mx-auto">
-							<div className="input-group my-3">
-								<label htmlFor="username" className="mr-2">username</label>
-								<input id="username" type="text" name="username" className="bg-gray-200 rounded-md p-1"/>
-							</div>							
-							<div className="input-group my-3">
-								<label htmlFor="email" className="mr-2">email</label>
-								<input id="email" type="text" name="email" className="bg-gray-200 rounded-md p-1"/>
-							</div>
-							<div className="input-group my-3">
-								<label htmlFor="password" className="mr-2">password</label>
-								<input id="password" type="text" name="password" className="bg-gray-200 rounded-md p-1"/>
-							</div>
-							<div className="input-group my-3">
-								<Link href="/app/initial-setup/form" className="bg-sky-500 rounded-md px-2 py-1 text-gray-100">Submit</Link>
-							</div>
-							<div className="text-container">
-								Already have account ? <Link href="/auth/login" className="text-sky-900">Let's login</Link>
-							</div>
-						</div>
+                        <form onSubmit={handleSubmit}>
+                            <div className="input-container text-center mx-auto">
+                                <div className="input-group my-3">
+                                    <label htmlFor="name" className="mr-2">username</label>
+                                    <input id="name" type="text" name="name" className="bg-gray-200 rounded-md p-1"/>
+                                </div>							
+                                <div className="input-group my-3">
+                                    <label htmlFor="email" className="mr-2">email</label>
+                                    <input id="email" type="text" name="email" className="bg-gray-200 rounded-md p-1"/>
+                                </div>
+                                <div className="input-group my-3">
+                                    <label htmlFor="password" className="mr-2">password</label>
+                                    <input id="password" type="text" name="password" className="bg-gray-200 rounded-md p-1"/>
+                                </div>
+                                <div className="input-group my-3">
+                                    <button type="submit" className="bg-sky-500 rounded-md px-2 py-1 text-gray-100">Submit</button>
+                                </div>
+                                <div className="text-container">
+                                    Already have account ? <Link href="/auth/login" className="text-sky-900">Let's login</Link>
+                                </div>
+                            </div>
+                        </form>
 					</div>
 				</div>
 			</div>
