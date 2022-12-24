92b1f72e2a068ee492cab0308f0d7f726f1fdcb4
Andika Wiratana <andikawiratana@gmail.com>
2022-12-24 19:31:28 +0800

    integrate food feature

--- pages/app/food/index.js
+++ pages/app/food/index.js
@@ -1,91 +1,90 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Image from 'next/image'
 
-const nutritionNeeds = [
-	{
-		id:0,
-		name:"carbohydrate",
-		amount:500,
-		unit:"grams",
-		foods: [
-			{	
-				id:0,
-				name:"potatoes",
-				image: "https://via.placeholder.com/50",
-				cal:240
-			},
-			{	
-				id:1,
-				name:"oats",
-				image: "https://via.placeholder.com/50",
-				cal:340
-			},
-			{	
-				id:2,
-				name:"rices",
-				image: "https://via.placeholder.com/50",
-				cal:210
-			},
-			{	
-				id:3,
-				name:"noodles",
-				image: "https://via.placeholder.com/50",
-				cal:140
-			},
-			{	
-				id:4,
-				name:"corn",
-				image: "https://via.placeholder.com/50",
-				cal:40
-			}
-		]
-	},
-	{
-		id:1,
-		name:"proteins",
-		amount:500,
-		unit:"grams",
-		foods: [
-			{	
-				id:0,
-				name:"Chicken Breast",
-				image: "https://via.placeholder.com/50",
-				cal:100
-			},
-			{	
-				id:1,
-				name:"Fish",
-				image: "https://via.placeholder.com/50",
-				cal:70
-			},
-			{	
-				id:2,
-				name:"Red meat",
-				image: "https://via.placeholder.com/50",
-				cal:120
-			},
-			{	
-				id:3,
-				name:"nuts",
-				image: "https://via.placeholder.com/50",
-				cal:300
-			},
-			{	
-				id:4,
-				name:"egg",
-				image: "https://via.placeholder.com/50",
-				cal:200
-			}
-		]
-	}
-];
-
 export default function Food(){
-	const [nutritions, setNutritions] = useState(new Map())
+	const [nutritions, setNutritions] = useState([])
+    const [nutritionLogs, setNutritionLogs] = useState([])
 	const [limit, setLimit]           = useState(0)
 	const [calories, setCalories]     = useState(0)
-
-	return (
+    const [bodyPreference, setBodyPreference] = useState({})
+    const [levelPack, setLevelPack] = useState({})
+    
+    const getBodyPreference = async () => {
+        await fetch(`http://localhost:5000/users/detail/${localStorage.getItem('user_id')}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'authorization': `Bearer ${localStorage.getItem("access-token")}`,
+          },
+        })
+        .then(async (res)=>{
+            await res.json().then(async (data)=>{
+                setLimit(data.detail.calories_need)
+                let level = data.detail.level
+                await fetch(`http://localhost:5000/body-pref/${data.detail.body_preference}`)
+                .then(async (res) => {
+                    await res.json().then(async (data)=> {
+                        setBodyPreference(data)
+                        setLevelPack(data.level_packs[level])
+                    })
+                })
+            })
+        })
+    }
+    
+    const handleSubmit = async () => {
+        const data = nutritions.map(e => {
+                e._userId = localStorage.getItem("user_id")
+                delete e._id
+                return e
+        })
+        const JSONdata = JSON.stringify(data)
+        await fetch('http://localhost:5000/food-log', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'authorization': `Bearer ${localStorage.getItem("access-token")}`,
+          },
+          body: JSONdata
+        }).then((res)=>{
+            loadNutritions()
+            setNutritions([])
+        })
+    }
+    
+    const deleteItem = async (nutrition) => {
+        await fetch(`http://localhost:5000/food-log/${nutrition._id}`, {
+          method: 'DELETE',
+          headers: {
+            'authorization': `Bearer ${localStorage.getItem("access-token")}`,
+          }
+        }).then(()=>loadNutritions())
+    }
+    
+    const loadNutritions = async () => {
+        await fetch('http://localhost:5000/food-log', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'authorization': `Bearer ${localStorage.getItem("access-token")}`,
+          },
+        })
+        .then(async (res)=>{
+            await res.json().then(async (data)=>{
+                if (data.length > 0) {
+                    setNutritionLogs(data)
+                    setCalories(data.map((e) => e.cal).reduce((a,b) => a+b))
+                }
+            })
+        })
+    }
+    
+    useEffect(()=>{
+        getBodyPreference()
+        loadNutritions()
+    },[])
+    
+	return levelPack.food_categories ? (
 		<section>
 			<div className="bg-form h-full bg-gray-800 flex items-center flex-col">
 				<div className="container mx-auto mt-[100px]">
@@ -95,12 +94,11 @@ export default function Food(){
 				</div>
 				<div className="container mx-auto my-[100px]">
 					<div className="flex-col form mx-auto w-2/3 bg-gray-100 p-5 rounded-xl shadow flex">
-						{nutritionNeeds.map(function(nutrition,i){
+                        {levelPack.food_categories.map(function(nutrition,i){
 							return (
 								<div key={nutrition.name} className={"card  group hover:w-full duration-700"}>
 									<div className={`flex m-[10px] p-[10px] hover:m-[0px] bg-gray-900 hover:p-[20px] hover:rounded-3xl duration-300 rounded-md align-center`}>
 										<div className="img-container w-fit">
-											<Image src="{nutrition.image}" alt=""/>
 										</div>
 										<div className="text-container text-sky-400 flex items-center">
 											<div className="title inline-block">{nutrition.name}</div>
@@ -110,14 +108,13 @@ export default function Food(){
 												return (
 													<div key={food.name}>
 														<div
-														className={`hidden m-[15px] ${nutrition[i] == food.id ? 'bg-sky-300' : 'bg-sky-900'} p-[10px] hover:m-[0px] hover:p-[20px] hover:rounded-3xl duration-300 rounded-md group-hover:block`}
-														onClick={()=> {												
-															setNutritions(new Map(nutritions.set(i, {id:food.id, cal:food.cal})))
-															setCalories([...nutritions.values()].reduce((a,b) => a + b.cal, 0))
+														className={`hidden m-[15px] bg-sky-900 p-[10px] hover:m-[0px] hover:p-[20px] hover:rounded-3xl duration-300 rounded-md group-hover:block`}
+														onClick={()=> {
+                                                            let arr = [...nutritions]
+                                                            arr.push(food)
+															setNutritions(arr)
+                                                            setCalories(arr.map((e)=>parseInt(e.cal)).reduce((a,b)=>a+b))
 														}}>
-															<div className="img-container w-fit">
-																<Image src="{food.image}" alt=""/>
-															</div>
 															<div className="text-container text-sky-400">
 																<div className="title">{food.name}</div>
 															</div>
@@ -132,33 +129,50 @@ export default function Food(){
 						})}
 					</div>
 				</div>
-				<div className="container mx-auto mb-[100px]">
-					<div className="flex-col form mx-auto w-2/3 bg-gray-100 p-5 rounded-xl shadow flex">
-						{[...nutritions.keys()].map(function(index){
-							return (
-								<div key={"item-"+index} className={"card  group hover:w-full duration-700"}>
-									<div className={`flex m-[10px] p-[10px] hover:m-[0px] bg-gray-900 hover:p-[20px] hover:rounded-3xl duration-300 rounded-md align-center flex-col`}>
-										<div className="img-container w-fit">
-											<Image src="" alt=""/>
-										</div>
-										<div className="text-container text-sky-400 flex items-center w-full justify-between">
-											<div className="title inline-block">{nutritionNeeds[index].foods[nutritions.get(index).id].name}</div>
-											<div className="delete-button"><button className="bg-sky-900 px-[10px] py-[5px] hover:rounded-3xl duration-300 rounded-md hover:bg-red-500 hover:text-white"
-												onClick={()=> {
-													setNutritions(() => {
-														nutritions.delete(index)
-														return new Map(nutritions)
-													})
-													setCalories([...nutritions.values()].reduce((a,b) => a + b.cal, 0))
-												}}>X</button></div>
-										</div>
-									</div>
-								</div>
-							)
-						})}
-					</div>
-				</div>
+                <div className="container mx-auto mb-[100px]">
+                    <div className="flex-col form mx-auto w-2/3 bg-gray-100 p-5 rounded-xl shadow flex">
+                    {console.log(nutritions)}
+                        {nutritions.map((nutrition, i) => {
+                            return (
+                                <div key={`${i}-{nutrition._id}`} className={"card  group hover:w-full duration-700"}>
+                                    <div className={`flex m-[10px] p-[10px] hover:m-[0px] bg-gray-900 hover:p-[20px] hover:rounded-3xl duration-300 rounded-md align-center flex-col`}>
+                                        <div className="img-container w-fit">
+                                        </div>
+                                        <div className="text-container text-sky-400 flex items-center w-full justify-between">
+                                            <div className="title inline-block">{nutrition.name}</div>
+                                            <div className="delete-button"><button className="bg-sky-900 px-[10px] py-[5px] hover:rounded-3xl duration-300 rounded-md hover:bg-red-500 hover:text-white" onClick={function(){
+                                                let arr = [...nutritions]
+                                                arr.splice(i, 1)
+                                                setNutritions(arr)
+                                                setCalories(calories-parseInt(nutrition.cal))
+                                            }}>X</button></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                        <button className="bg-sky-900 px-[10px] py-[5px] hover:rounded-3xl duration-300 rounded-md hover:bg-red-500 hover:text-white" onClick={handleSubmit}>Save</button>
+                    </div>
+                </div>
+                <div className="container mx-auto mb-[100px]">
+                    <div className="flex-col form mx-auto w-2/3 bg-gray-100 p-5 rounded-xl shadow flex">
+                        {nutritionLogs.map((nutrition, i) => {
+                            return (
+                                <div key={`${i}-{nutrition._id}`} className={"card  group hover:w-full duration-700"}>
+                                    <div className={`flex m-[10px] p-[10px] hover:m-[0px] bg-gray-900 hover:p-[20px] hover:rounded-3xl duration-300 rounded-md align-center flex-col`}>
+                                        <div className="img-container w-fit">
+                                        </div>
+                                        <div className="text-container text-sky-400 flex items-center w-full justify-between">
+                                            <div className="title inline-block">{nutrition.name}</div>
+                                            <div className="delete-button"><button className="bg-sky-900 px-[10px] py-[5px] hover:rounded-3xl duration-300 rounded-md hover:bg-red-500 hover:text-white" onClick={()=>deleteItem(nutrition)}>X</button></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
 			</div>
 		</section>
-	)
+	) : (<div>Hallo</div>)
 }
