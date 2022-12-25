958b975be58124b14d8d49064340140326301c6f
Andika Wiratana <andikawiratana@gmail.com>
2022-12-26 02:19:45 +0800

    update app index page style

--- pages/app/index.js
+++ pages/app/index.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import Image from 'next/image'
 
 export default function Dashboard(){
     const router = useRouter()
@@ -33,14 +34,33 @@ export default function Dashboard(){
     
 	return (
 		<section>
-			<div className="bg-form h-screen bg-gray-800 flex items-center flex-col">
-				<div className="container mx-auto my-[100px]">
+			<div className="bg-form h-screen base-background flex items-center flex-col">
+				<div className="container mx-auto my-5">
 					<div className="flex-col form mx-auto w-2/3 bg-gray-100 p-5 rounded-xl shadow flex">
-						<div className="w-1/2 p-[20px] bg-sky-500 text-white rounded mx-auto text-center my-[20px]">{cal}/{calNeed}</div>
+                        <div className="text-container text-center">
+                            <Image src="/assets/img/Logo.png" width={75} height={75} className="mx-auto"/>
+                            <h1 className="text-sky-900 text-2xl">Welcome To FitStudee</h1>
+                        </div>
+						<div className="w-1/2 p-[20px] base-background text-white rounded mx-auto text-center my-[20px]">{cal}/{calNeed}</div>
 						<div className="text-container flex justify-around">
-							<Link href="/app/food" className="bg-sky-900 px-[10px] py-[5px] hover:rounded-3xl duration-300 rounded-md hover:bg-red-500 text-white">Food</Link>
-				            <Link href="/app/water" className="bg-sky-900 px-[10px] py-[5px] hover:rounded-3xl duration-300 rounded-md hover:bg-red-500 text-white">Water</Link>
-				            <Link href="/app/excercise" className="bg-sky-900 px-[10px] py-[5px] hover:rounded-3xl duration-300 rounded-md hover:bg-red-500 text-white">Excercise</Link>
+                            <Link href="/app/food" className={`block shadow rounded-md overflow-hidden m-[10px] base-background card w-1/3 pb-[10px] hover:drop-shadow-2xl duration-300`}>
+                                <Image src="/assets/img/example.jpeg" width={300} height={300} className="object-fill"/>
+                                <div className="text-container p-[5px_10px] text-xl text-slate-50 font-semibold text-center">
+                                    Food
+                                </div>
+                            </Link>
+                            <Link href="/app/excercise" className={`block shadow rounded-md overflow-hidden m-[10px] base-background card w-1/3 pb-[10px] hover:drop-shadow-2xl duration-300`}>
+                                <Image src="/assets/img/example.jpeg" width={300} height={300} className="object-fill"/>
+                                <div className="text-container p-[5px_10px] text-xl text-slate-50 font-semibold text-center">
+                                    Excercise
+                                </div>
+                            </Link>
+                            <Link href="/app/water" className={`block shadow rounded-md overflow-hidden m-[10px] base-background card w-1/3 pb-[10px] hover:drop-shadow-2xl duration-300`}>
+                                <Image src="/assets/img/example.jpeg" width={300} height={300} className="object-fill"/>
+                                <div className="text-container p-[5px_10px] text-xl text-slate-50 font-semibold text-center">
+                                    Water
+                                </div>
+                            </Link>
 		        		</div>
 					</div>
 				</div>
