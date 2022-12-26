ed7e0b59337ac61c9ef80b7f57e2f3ec11e9fecf
Andika Wiratana <andikawiratana@gmail.com>
2022-12-27 01:32:29 +0800

    update water layout

--- pages/app/water/index.js
+++ pages/app/water/index.js
@@ -24,7 +24,8 @@ export default function Water(){
             }else{
                 await res.json().then(e => {
                     setLogs(e)
-                    setTotal(e.map(e => parseInt(e.amount)).reduce((a,b)=> a + b))
+                    if(e.length > 0)
+                        setTotal(e.map(e => parseInt(e.amount)).reduce((a,b)=> a + b))
                 })
             }
         })
@@ -60,30 +61,33 @@ export default function Water(){
 
 	return (
 		<section>
-			<div className="bg-form h-full bg-gray-800 flex items-center flex-col">
-				<div className="container mx-auto my-[100px]">
+                        <button className={`w-[50px] h-[50px] base-background absolute left-[5px] top-[5px] rounded-full text-slate-50 shadow text-lg`} onClick={()=>router.back()}>Back</button>
+			<div className={`bg-form ${logs.length > 2 ? 'h-full' : 'h-screen'} base-background flex items-center flex-col`}>
+				<div className="container mx-auto p-5">
 					<div className="flex-col form mx-auto w-2/3 bg-gray-100 p-5 rounded-xl shadow flex">
-						<div className="w-1/2 p-[20px] bg-sky-500 text-white rounded mx-auto text-center my-[20px]">{`${total}/${limit}`}</div>
-						<div className="input-group my-3 mx-auto">
-							<input id="Unit" type="text" name="Unit" className="border-b-4 border-indigo-500 p-1 text-center bg-transparent" value={unit} onChange={event => setUnit(event.target.value)}/>
+                        <div className="text-container text-center">
+                            <Image src="/assets/img/Logo.png" width={75} height={75} className="mx-auto"/>
+                            <h1 className="text-sky-900 text-2xl">FitStudee</h1>
+                        </div>
+						<div className="w-1/2 p-[20px] base-background text-white rounded mx-auto text-center my-[20px]"><span className="text-2xl">&#128167;</span> Konsumsi Air : <span className="text-lg font-bold">{total}</span>/<span className="text-md">{limit}</span></div>
+						<div className="input-group my-3 mx-auto flex mb-[50px] m-[10px]">
+                            <input id="Unit" type="text" name="Unit" className="rounded-bl-lg rounded-tl-lg border-2 border-indigo-500 p-1 text-center bg-transparent" value={unit} onChange={event => setUnit(event.target.value)}/>
+                            <button className="rounded-tr-lg rounded-br-lg p-[10px] base-background hover:bg-gray-500 duration-300 align-center text-white w-1/3 mx-auto text-lg" onClick={(e) => handleSubmit() }>&#10133;</button>
 						</div>
-						<button className="m-[10px] p-[10px] bg-gray-900 hover:bg-gray-500 duration-300 rounded-md align-center text-white w-1/3 mx-auto" onClick={(e) => handleSubmit() }>Tambah</button>
-					</div>
-				</div>
-				<div className="container mx-auto mb-[100px]">
-					<div className="flex-col form mx-auto w-2/3 bg-gray-100 p-5 rounded-xl shadow flex">
-						{logs.map(function(e){
-							return (
-								<div key={e._id} className={"card  group hover:w-full duration-700"}>
-									<div className={`flex m-[10px] p-[10px] hover:m-[0px] bg-gray-900 hover:p-[20px] hover:rounded-3xl duration-300 rounded-md align-center flex-col`}>
-										<div className="text-container text-sky-400 flex items-center w-full justify-between">
-											<div className="title inline-block"><strong>{e.amount} Ml</strong>&emsp;{e.created_at}</div>
-											<div className="delete-button"><button className="bg-sky-900 px-[10px] py-[5px] hover:rounded-3xl duration-300 rounded-md hover:bg-red-500 hover:text-white" onClick={() => deleteItem(e._id)}>X</button></div>
-										</div>
-									</div>
-								</div>
-							)
-						})}
+                        <div className="flex-col flex">
+                            {logs.map(function(e){
+                                return (
+                                    <div key={e._id} className={"card  group hover:w-full duration-700"}>
+                                        <div className={`flex m-[10px] p-[10px] hover:m-[0px] bg-gray-900 hover:p-[20px] hover:rounded-3xl duration-300 rounded-md align-center flex-col`}>
+                                            <div className="text-container text-sky-400 flex items-center w-full justify-between">
+                                                <div className="title inline-block"><span className="text-2xl">&#128167;</span> Jumlah Konsumsi : <span className="text-lg font-bold">{e.amount} ML</span>&emsp;&emsp;<span className="text-md">&#128336; {e.created_at}</span></div>
+                                                <div className="delete-button"><button className="bg-sky-900 px-[10px] py-[5px] hover:rounded-3xl duration-300 rounded-md hover:bg-red-500 hover:text-white" onClick={() => deleteItem(e._id)}>X</button></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            })}
+                        </div>
 					</div>
 				</div>
 			</div>
